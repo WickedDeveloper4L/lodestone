e9e34681f1fb1dd547d19cc47877ca250b879418
seyi-dev1 <samoluwaseyi25gmail.com>
2023-07-09 18:04:54 +0100

    adding footer

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero/Hero";
 import React from "react";
 import Sponsor from "@/components/Sponsor/Sponsor";
+import Footer from "@/components/Footer/Footer";
 const Home = () => {
   return (
     <div>
       <Hero />
       <Sponsor />
+      <Footer />
     </div>
   );
 };
---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import styles from "./footer.module.scss";
+import React from "react";
+import logo from "../../assets/logoo.png";
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.credits}>
+        <p>© Copyright 2022 Lodstone Medicals Limited</p>
+      </div>
+      <div className={styles.logo}>
+        <Image src={logo} />
+      </div>
+      <div className={styles.links}>
+        <p>Privacy policy</p>
+        <p>Cookies policy</p>
+        <p>Terms and Conditions</p>
+        <p>Modern Slavery Act Statement</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
