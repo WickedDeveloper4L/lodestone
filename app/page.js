206f54d4a56cb6733c54276d0726b182e7776870
Ahia Kleine Chris <wickeddeveloper4l@gmail.com>
2023-07-09 04:01:59 +0100

    making changes

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
+import Hero from "@/components/Hero/Hero";
 import React from "react";
-import styles from "./page.module.css";
+
 const Home = () => {
-  return <div className={styles.main}>Home</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 
 export default Home;
