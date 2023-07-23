014b23ded84088a5cab9ac8d0e489793b76edc95
Ahia Kleine Chris <wickeddeveloper4l@gmail.com>
2023-07-23 08:33:00 +0100

    added meta tags

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,17 @@ export const roboto_mono = Roboto_Mono({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="title" content="Lodestone Patient care Hospital" />
+        <meta
+          name="description"
+          content="Welcome to Lodestone patient care Hospital - where compassionate care meets cutting-edge medicine. Our dedicated team of professionals provides personalized treatment in a welcoming and modern facility. Your health and well-being are our priority, ensuring a comforting and healing experience throughout your journey to recovery. Lodestone patient care hospital for exceptional healthcare and support."
+        />
+        <meta name="keywords" content="Hospital, Patient, NHS, health, MRi" />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+      </head>
       <body className={roboto.className}>
         <Navbar />
         <div>{children}</div>
